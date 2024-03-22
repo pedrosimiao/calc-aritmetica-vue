82ea92123bc459f6fb59f26d9c3c4ef2e6a3bb5a
pedrosimiao <pedrolumiere@gmail.com>
2024-03-22 06:51:20 -0300

    calculadora aritmética com VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+  const dadosReativos = reactive({
+    operacao: 'soma',
+    operador: '+',
+    operando1: 0,
+    operando2: 0,
+    resultado: 0
+  })
+
+  const trocarOperacao = () => {
+    const { operacao, operando1, operando2 } = dadosReativos;
+    
+    switch (operacao) {
+      case 'soma':
+        dadosReativos.operador = '+';
+        dadosReativos.resultado = operando1 + operando2;
+        break
+      case 'subtracao':
+        dadosReativos.operador = '-';
+        dadosReativos.resultado = operando1 - operando2;
+        break
+      case 'multiplicacao':
+        dadosReativos.operador = '*';
+        dadosReativos.resultado = operando1 * operando2;
+        break
+      case 'divisao':
+        dadosReativos.operador = '÷';
+        if (operando2 !== 0) {
+        dadosReativos.resultado = operando1 / operando2;
+      } else {
+        dadosReativos.resultado = 0;
+      }
+        break
+      default:
+        dadosReativos.operador = '';
+        dadosReativos.resultado = 0;
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-dark rounded-3">
+        <h1 class="text-light text-center">Calculadora Aritmética</h1>
+    </header>
+    <form>
+        <div class="row">
+          <div class="col-md-3">
+            <select v-model="dadosReativos.operacao" @change="trocarOperacao" class="form-control"> <!-- Usar o @change -->
+              <option value="soma">Soma</option>
+              <option value="subtracao">Subtração</option>
+              <option value="multiplicacao">Multiplicação</option>
+              <option value="divisao">Divisão</option>
+            </select>
+          </div>
+          <div class="col-md-3">
+            <input type="number" placeholder="0" class="form-control" v-model="dadosReativos.operando1" @keyup="trocarOperacao">
+          </div>
+          <div class="text-center col-md-1">
+            <span class="h3">
+              {{ dadosReativos.operador }}
+            </span>
+          </div>
+          <div class="col-md-3">
+            <input type="number" placeholder="0" class="form-control" v-model="dadosReativos.operando2" @keyup="trocarOperacao">
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col-md-6">
+            <h4>Primeiro operando: {{ dadosReativos.operando1 }}</h4>
+            <br>
+            <h4>Segundo operando: {{ dadosReativos.operando2 }}</h4>
+            <br>
+            <h4>Resultado: {{ dadosReativos.resultado }}</h4>    
+          </div>
+        </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
